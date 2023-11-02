b108c0a02119c4b9cf0ff25c29a939953c469c15
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2023-11-02 12:50:02 +0100

    feat: handling unhandled routes

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -28,6 +23,13 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 // 4. START SERVER
 module.exports = app;
