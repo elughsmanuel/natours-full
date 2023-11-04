e2c653131d5d8ebab2753ca74ca485c7eed6b29f
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2023-11-04 18:01:54 +0100

    feat: handling duplicate database fields and validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -3,7 +3,23 @@ const AppError = require('../utils/appError');
 const handleCastErrorDB = err => {
   const message = `Invalid ${err.path}: ${err.value}.`;
   return new AppError(message, 400);
-}
+};
+
+// const handleDuplicateFieldsDB = err => {
+//   const value = err.errmsg.match(/[" '](\\?.)*?\1/)[0];
+//   console.log(value);
+
+//   const message = `Duplicate field value: ${value}. Please use another value!`;
+//   return new AppError(message, 400);
+// };
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.error).map(el => el.messsage);
+
+  const message = `Invalid input data. ${errors.join(' ')}`;
+  return new AppError(message, 400);
+};
+
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -52,6 +68,8 @@ module.exports = (err, req, res, next) => {
       let error = { ...err };
 
       if(error.name === 'CastError') error = handleCastErrorDB(error);
+      // if(error.code === 11000) error = handleDuplicateFieldsDB(error);
+      if(error.name === 'ValidationError') error = handleValidationErrorDB(error);
 
       sendErrorProd(error, res);
     }
